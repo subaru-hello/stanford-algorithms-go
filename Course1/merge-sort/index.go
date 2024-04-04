a0190e21530b4a770756df7dd3accf40dcfb2f63
subaru-hello <octosubaru0926@gmail.com>
2024-04-05 08:41:02 +0900

    feat: merge_sort

--- Course1/merge-sort/index.go
+++ Course1/merge-sort/index.go
@@ -1,19 +1,82 @@
 package main
 
-// split arrays into two(at splitting process, the arrays are sorted orderly)
-func splitIntoTwoStrings(){
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
+func mergeSort(items []int)[]int{
+	// base case
+if(len(items) < 2){
+	return items
+}
+first := mergeSort(items[:len(items)/2])
+second := mergeSort(items[len(items)/2:])
+return merge(first, second)
 }
 
-func sort(){
+// after sorted and devided into one piece, merge them all into one array.
+func merge(x,y []int) []int {
+	finalOutput := []int{}
+	i := 0
+	j := 0
 
+	for i < len(x) && j < len(y) {
+		if x[i] < y[j] {
+			finalOutput = append(finalOutput, x[i])
+			i++
+		} else {
+			finalOutput = append(finalOutput, y[j])
+			j++
+		}
+	}
+
+	// Append remaining elements of x
+	for ; i < len(x); i++ {
+		finalOutput = append(finalOutput, x[i])
+	}
+	// Append remaining elements of y (fixed: changed i to j)
+	for ; j < len(y); j++ {
+		finalOutput = append(finalOutput, y[j])
+	}
+	return finalOutput
 }
 
-// merge
-func merge(){
-	
+func readIntegersFromFile(fileName string) ([]int, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var integers []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		num, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return nil, err
+		}
+		integers = append(integers, num)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return integers, nil
 }
 
 func main(){
+	fileName := "integerArray.txt" // Replace with your file's path
+	integers, err := readIntegersFromFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+    // unsorted := []int{10, 6, 2, 1, 5, 8, 3, 4, 7, 9}
+    sorted := mergeSort(integers)
+	fmt.Println(sorted)
 }
